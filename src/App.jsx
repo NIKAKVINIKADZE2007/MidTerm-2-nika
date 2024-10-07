e096af58a7ade978de5a501e1eae11bfcda4a506
nikakvinikadze <nikakvinikadze2007@gmail.com>
2024-10-07 22:55:51 +0400

    adding bugged

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
-  return (
-    <>
-      <h1 className="text-3xl font-bold text-center">Midterm 2</h1>
-    </>
-  );
+  const [info, setInfo] = useState([]);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    axios.get('https://randomuser.me/api/?results=10').then((data) => {
+      setInfo(data.data.results);
+      setLoading(false);
+    });
+  }, []);
+
+  if (loading) {
+    return <p>loading</p>;
+  } else {
+  }
+  return <>{info && info.map((person) => console.log(person.gender))}</>;
 }
 
 export default App;
