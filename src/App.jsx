635bd42f78d3a72c3818e9b4d25a6150e95b4928
nikakvinikadze <nikakvinikadze2007@gmail.com>
2024-10-07 22:13:38 +0400

    practise-1

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,44 @@
-import React from 'react';
+import { useState } from 'react';
 
 function App() {
+  const [todoText, setTodoText] = useState('');
+  const [todos, setTodos] = useState([]);
+
+  const handleChange = (text) => {
+    setTodoText(text.target.value);
+    console.log(todoText);
+  };
+
+  const handleClick = (event) => {
+    event.preventDefault();
+    setTodos((prev) => [...prev, todoText]);
+  };
   return (
     <>
-      <h1 className="text-3xl font-bold text-center">Midterm 2</h1>
+      <form>
+        <label htmlFor='todo'>enter Todo text</label>
+        <input
+          className='border-black border-[2px]'
+          id='todo'
+          type='text'
+          onChange={(event) => handleChange(event)}
+        />
+        <button
+          className='block  bg-red-500 w-[100px] h-[20px]'
+          type='submit'
+          onClick={(event) => {
+            handleClick(event);
+          }}
+        >
+          Add todo
+        </button>
+      </form>
+
+      <div>
+        {todos.map((todo) => {
+          return <p>{todo}</p>;
+        })}
+      </div>
     </>
   );
 }
